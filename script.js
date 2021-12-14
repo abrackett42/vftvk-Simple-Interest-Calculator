f33d62a5c23333e070f02931f2fb64af785cf11b
abrackett42 <abrackett42@gmail.com>
2021-12-14 21:55:49 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function compute()
 {
     //input variables
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
@@ -9,7 +8,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var result = interest;
     
-    document.getElementById("result").innerHTML="If you deposit $'<mark>'"+principal+"'</mark>',\<br\>at an interest rate of '<mark>'"+rate+"'</mark>'%\<br\>You will receive an interest amount of $'<mark>'"+result+"'</mark>',\<br\>in the year '<mark>'"+year+"'</mark>'\<br\>"
+    document.getElementById("result").innerHTML="If you deposit $'<mark>'"+principal+"'</mark>',\<br\>at an interest rate of '<mark>'"+rate+"'</mark>'%\<br\>You will receive an amount of $'<mark>'"+result+"'</mark>',\<br\>in the year '<mark>'"+year+"'</mark>'\<br\>"
 }
 
 function updateRate() //input function that reads value of the range slider
@@ -21,10 +20,16 @@ function updateRate() //input function that reads value of the range slider
 //input a function that validatees the Principal input box to a number higher than zero
 function validateAmount()
 {
-    var principal = document.getElementById("principal").value;
-    var higherthanzero = parseInt(principal) > 0;
-    if (!higherthanzero) {
-        alert("Please enter a positive number higher than 0!");
-        document.getElementById("principal").focus();
+    var principal = document.getElementById("principal");
+    
+    if(principal.value == ""){
+        alert("PLEASE ENTER NUMBER HIGHER THAN 0")
+        principal.focus();
+        return false
+    }
+    if(principal.value < 0){
+        alert("PLEASE ENTER NUMBER HIGHER THAN 0")
+        prinicipal.focus()
+        return false
     }
 }
